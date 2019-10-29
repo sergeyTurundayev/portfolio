0ca21a5fe41797686c6600e2446f2ac12a6a7229
Abdullah Turundayev <abdullah@MacBook-Abdullah.local>
2019-10-29 16:05:30 +0200

    move file send.js

--- index.php
+++ index.php
@@ -83,6 +83,6 @@
 <script src="js/jquery.min.js"></script>
 <script src='js/scrollTo.js'></script>
 <script src="js/main.js"></script>
-<script src="js/send-message.js"></script>
+<script src="sends/send-message.js"></script>
 </body>
 </html>
