6ccf9cf3ee98221c877cd908b02cfa2f0dbf6753
Abdullah Turundayev <abdullah@you.pc.local>
2020-02-11 15:35:17 +0200

    changes

--- sends/form.php
+++ sends/form.php
@@ -1,11 +1,18 @@
 <?php
+    $email_to =   'sergey.turundayev@gmail.com';
+    // $name     =   $_POST['name'];
+    // $email    =   $_POST['email'];
+    // $subject  =   $_POST['subject'];
+    // $message  =   'Email: '.$_POST['email'].' Сообщение: '.$_POST['message'];
+    
+    // $headers  = "From: herokuapp \r\n";
+    // $headers .= "Reply-To: $email\r\n";
+    
+    if(mail($email_to, 'subject', 'message' )){ // $subject, $message, $headers
+        echo 'true';
+    }else{
+        echo 'error';
+    }
 
-if ( mail('sergey.turundayev@gmail.com', 'subject', 'message') ){
-	echo "true";
-} else{
-	echo "error";
-}
-
-exit();
-
+    exit();
 ?>
