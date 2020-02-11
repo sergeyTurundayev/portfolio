b6233d57672c140a882896e208c4a0f7e299ccb2
Abdullah Turundayev <abdullah@MacBook-Abdullah.local>
2020-02-11 13:30:23 +0200

    change php

--- sends/send_email.php
+++ sends/send_email.php
@@ -1,15 +1,14 @@
 <?php
-
-    $email_to =   'abdullah.turundayev@gmail.com';
+    $email_to =   'sergey.turundayev@gmail.com'; // 7657789@gmail.com
     $name     =   $_POST['name'];
     $email    =   $_POST['email'];
     $subject  =   $_POST['subject'];
-    $message  =   $name . ". " . $_POST['message'];
+    $message  =   'Email: '.$_POST['email'].' Сообщение: '.$_POST['message'];
     
-    // $headers  = "From: herokuapp \r\n";
-    // $headers .= "Reply-To: $email\r\n";
+    $headers  = "From: web-shop.tk \r\n";
+    $headers .= "Reply-To: $email\r\n";
     
-    if( mail($email_to, $subject, $message) ){ // , $headers
+    if(mail($email_to, $subject, $message, $headers)){
         echo 'sent';
     }else{
         echo 'failed';
