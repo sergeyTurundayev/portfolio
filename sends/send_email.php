7baf67661fc8f974e186e92086b045fe8e3f8ab5
Abdullah Turundayev <abdullah@you.pc.local>
2020-02-11 14:32:58 +0200

    change php

--- sends/send_email.php
+++ sends/send_email.php
@@ -1,20 +1,7 @@
 <?php
     
     mail('sergey.turundayev@gmail.com', 'subject', 'message');
-    echo ":)";
+    echo "line 4";
 
-    $email_to =   'sergey.turundayev@gmail.com'; // 7657789@gmail.com
-    $name     =   $_POST['name'];
-    $email    =   $_POST['email'];
-    $subject  =   $_POST['subject'];
-    $message  =   'Email: '.$_POST['email'].' Сообщение: '.$_POST['message'];
-    
-    $headers  = "From: web-shop.tk \r\n";
-    $headers .= "Reply-To: $email\r\n";
-    
-    if(mail($email_to, $subject, $message, $headers)){
-        echo 'sent';
-    }else{
-        echo 'failed';
-    }
+    exit();
 ?>
